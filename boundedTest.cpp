f708d83946aca90cc59f4c47252efba7954c0471
Abdurisaq <AbdurisaqH@outlook.com>
2023-06-01 11:36:30 -0700

    the first commit

---
+++ boundedTest.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class displacement{
+    int x;
+    int y;
+    int direction;
+    public:
+
+    displacement(){
+        x=0;
+        y=0;
+        direction=0;
+    }
+    string get_position(){
+        return "("+to_string(x)+","+to_string(y)+")";
+    }
+    //GLGL
+
+};
+
+
+int main(){
+    string command1= "GGLLGG";
+    string command2= "GG";
+    string command3= "GL";
+ 
+
+}
